9b5ff7b78cb35b213a8b01b4b9b6ed67eaa33209
LHT-Stan <lihongting_email@163.com>
2024-08-10 09:07:27 +0000

    日历 UTC 2024-08-10 Saturday 09:07:27 Updated By Github Actions

---
+++ js/diytitle.js
@@ -0,0 +1,20 @@
+/**
+ * 动态title
+ */
+ var OriginTitile = document.title;
+ var titleTime;
+ document.addEventListener('visibilitychange', function () {
+     if (document.hidden) {
+         //离开当前页面时标签显示内容
+         document.title = '♪(^∇^*)！慢走不送！';
+         clearTimeout(titleTime);
+     }
+     else {
+         //返回当前页面时标签显示内容
+         document.title = '♪(^∇^*)！欢迎回来！' + OriginTitile;
+         //两秒后变回正常标题
+         titleTime = setTimeout(function () {
+             document.title = OriginTitile;
+         }, 2000);
+     }
+ });
